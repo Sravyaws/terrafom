6ad1a5f2b78127617126228e802b4f7432d1a5f8
Sravyaws <Sravyaguduru18@gmail.com>
2023-07-18 14:52:36 +0530

    added new changes

--- providers.tf
+++ providers.tf
@@ -9,7 +9,5 @@ terraform {
 
 # configure aws
 provider "aws" {
-      region      = "ap-south-1"
-      access_key  = "AKIAS6VLD4O6PCGMRFPM"
-      secret_key  = "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"  
+      region      = "ap-south-1" 
   }

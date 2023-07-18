528cdcee6a68c86df93e5c61e43904bd64c630f2
Sravyaws <Sravyaguduru18@gmail.com>
2023-07-18 14:51:22 +0530

    Added changes

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-# configure aws
-provider "aws" {
-      region      = "ap-south-1"
-      access_key  = "AKIAS6VLD4O6PCGMRFPM"
-      secret_key  = "/ru0/+NCBiPV9ZyI86mkNZatGpsyi4D1RuJkZWHi"  
-  }
-
 # creating s3 bucket
 resource "aws_s3_bucket" "sample" {
   bucket = "gs123"
